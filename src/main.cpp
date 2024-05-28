899340616451211fd42ccc16287a31319f1a44ff
John Doe <johndoe@mail.com>
2024-05-28 19:51:16 -0400

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,8 @@ static Game game;
 static void *mainThread(void *);
 static MyDisplay myDisplay;
 
-int main(void) {
+int main(void)
+{
   pthread_t threadID;
   char str[1024];
 
@@ -33,7 +34,8 @@ int main(void) {
   openGame(&game, std::string("hl2_linux").data());
   pthread_create(&threadID, NULL, mainThread, NULL);
 
-  while (1) {
+  while (1)
+  {
     fputs("> ", stdout);
 
     if (fgets(str, 1024, stdin))
@@ -42,13 +44,17 @@ int main(void) {
 }
 
 /* Main cheat thread */
-static void *mainThread(void *_) {
+static void *mainThread(void *_)
+{
   std::string signature = "C8 4B ?? ?? C0 F2 ?? ?? 01 00 00 00 28 88 ?? ?? 70 8C ?? ?? 0A 40 00 00 30 DE ?? ?? C0 37 ?? ?? 02 ?? ?? ??";
   u_int32_t result = sigScan(game.pid, game.ClientModuleEnd, game.EngineModule, signature);
   u_int32_t forceJumpAddress;
-  if (result == -1) {
+  if (result == -1)
+  {
     die("forceJumpAddress signature not found");
-  } else {
+  }
+  else
+  {
     forceJumpAddress = result - 0xA0;
     std::cout << "forceJumpAddress signature found at address: " << std::hex << forceJumpAddress << std::endl;
   }
@@ -65,27 +71,28 @@ static void *mainThread(void *_) {
   if ((uinputF = openUinputKeyboard()) == -1)
     die(std::string("Could not open /dev/uinput").data());
 
-  while (checkGame(game.pid) != -1) {
+  while (checkGame(game.pid) != -1)
+  {
     if (playerFound(&game) == -1)
       continue;
 
     if (myDisplay.keyDown("XK_space") &&
-        (
-          game.player.m_fFlags == 129 || game.player.m_fFlags == 641 ||
-          game.player.m_fFlags == 131 || game.player.m_fFlags == 643
-        )) {
+        (game.player.m_fFlags == 129 || game.player.m_fFlags == 641 ||
+         game.player.m_fFlags == 131 || game.player.m_fFlags == 643))
+    {
 
-      //if (!game.options.BhopDelay || rand() % 50 <= 10) {
-        // Do the bhop faggot
-        ptraceWrite(game.pid, forceJumpAddress, "\x05", 1);
-        usleep(10000);
+      // if (!game.options.BhopDelay || rand() % 50 <= 10) {
+      //  Do the bhop faggot
+      ptraceWrite(game.pid, forceJumpAddress, "\x05", 1);
+      usleep(10000);
       //}
     }
-    else {
-        ptraceWrite(game.pid, forceJumpAddress, "\x04", 1);
+    else
+    {
+      ptraceWrite(game.pid, forceJumpAddress, "\x04", 1);
     }
 
-    //sleep(1);
+    // sleep(1);
   }
 
   die(std::string("Game not running").data());
